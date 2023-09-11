bca540554d43edd2075162518d5960f2a542f9f8
Amirmohamad <a.hejazirad@dornica.net>
2023-09-11 14:34:04 +0330

    initial cmt

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var Size;
+(function (Size) {
+    Size[Size["Small"] = -9] = "Small";
+    Size[Size["Medium"] = -8] = "Medium";
+    Size[Size["Large"] = -7] = "Large";
+    Size[Size["x"] = -6] = "x";
+    Size[Size["t"] = -5] = "t";
+    Size[Size["y"] = -4] = "y";
+    Size[Size["u"] = -3] = "u";
+    Size[Size["q"] = -2] = "q";
+    Size[Size["a"] = -1] = "a";
+    Size[Size["p"] = 0] = "p";
+})(Size || (Size = {}));
+;
+let mySize = Size.p;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+enum Size {Small=-9 , Medium , Large , x, t , y, u, q,a,p};
+
+let mySize : Size = Size.p;
+
+console.log(mySize);
