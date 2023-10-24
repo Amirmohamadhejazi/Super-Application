0468db2d5af9a63a1f926e2b08e0dfddf15bcfbc
Amirmohamad <Amirmohamadhejazi.9731@gmail.com>
2023-10-24 16:17:36 +0330

    fix bugs

--- src/app/components/TodoApp/TodoApp.tsx
+++ src/app/components/TodoApp/TodoApp.tsx
@@ -4,16 +4,20 @@ import { toast } from 'react-toastify';
 
 import { AnimatePresence, motion } from 'framer-motion';
 import { BsFillTrash3Fill } from 'react-icons/bs';
+import { AiOutlineStar } from 'react-icons/ai';
 
 interface ITodo {
     nameTodo: string;
     createTime: string;
+    important: string;
     id: number;
 }
 
 const TodoApp = () => {
     const [todo, setTodo] = useState<ITodo[]>([]);
     const [task, setTask] = useState('');
+    const [selectImportant, setSelectImportant] = useState<string>('1');
+    console.log(todo);
 
     const addTodo = (data: any) => {
         data.preventDefault();
@@ -25,6 +29,7 @@ const TodoApp = () => {
             const newData: ITodo = {
                 nameTodo: task,
                 createTime: dateCreate,
+                important: selectImportant ? selectImportant : '1',
                 id: new Date().getTime()
             };
             setTask('');
@@ -49,7 +54,7 @@ const TodoApp = () => {
     };
 
     return (
-        <div className="h-screen w-1/2 flex flex-col ">
+        <div className="h-screen w-1/2 flex flex-col items-center ">
             <h1 className="text-3xl font-semibold text-center mb-5 text-blue-600">To-Do List</h1>
 
             <form className="flex space-x-2 whitespace-nowrap" onSubmit={(event) => addTodo(event)}>
@@ -60,6 +65,18 @@ const TodoApp = () => {
                     value={task}
                     onChange={(event) => setTask(event.target.value)}
                 />
+                <select
+                    className="select select-bordered w-full max-w-xs"
+                    defaultValue={selectImportant}
+                    onChange={(e) => setSelectImportant(e.target.value)}
+                >
+                    <option disabled selected>
+                        How important is it?
+                    </option>
+                    <option value={1}>It is important</option>
+                    <option value={2}>Its importance is moderate</option>
+                    <option value={3}>No matter</option>
+                </select>
                 <motion.button
                     whileHover={{ scale: 1.05 }}
                     whileTap={{ scale: 0.9 }}
@@ -78,20 +95,32 @@ const TodoApp = () => {
                     Delete All
                 </motion.button>
             </form>
-            <div className="mt-5 flex flex-grow  flex-col gap-y-4 pr-2 overflow-y-auto ">
+            <div className="mt-5 flex flex-grow  flex-col gap-y-4  overflow-y-auto text-white">
                 <AnimatePresence>
-                    {todo.map((todoItems, index) => (
+                    {todo.map((todoItems) => (
                         <motion.div
                             layout
                             animate={{ scale: 1, opacity: 1 }}
                             exit={{ scale: 0.8, opacity: 0 }}
                             transition={{ type: 'spring' }}
                             key={todoItems.id}
-                            className={`grid grid-cols-3 bg-[#3C486B] items-center px-3 py-3 rounded-md gap-x-10 `}
+                            className={`grid grid-cols-3  items-center px-3 py-3 bg-[#3C486B] rounded-md gap-x-10 `}
                         >
-                            <span className="text-lg text-ellipsis overflow-hidden" title={todoItems.nameTodo}>
-                                {todoItems.nameTodo}
-                            </span>
+                            <div
+                                className="flex items-center gap-x-1 text-lg text-ellipsis overflow-hidden"
+                                title={todoItems.nameTodo}
+                            >
+                                <AiOutlineStar
+                                    className={`text-lg  ${
+                                        todoItems.important === '1'
+                                            ? 'text-yellow-400'
+                                            : todoItems.important === '2'
+                                            ? 'text-yellow-600'
+                                            : todoItems.important === '3' && 'text-yellow-800'
+                                    }`}
+                                />
+                                <span>{todoItems.nameTodo}</span>
+                            </div>
                             <span
                                 className="text-sm text-gray-400 text-ellipsis overflow-hidden"
                                 title={todoItems.createTime}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './globals.css';
 import { Inter } from 'next/font/google';
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] });
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={`${inter.className}  select-none bg-black text-white`}>
+            <body className={`${inter.className}  select-none  `}>
                 <div className=" container mx-auto">{children}</div>
                 <ToastContainer />
             </body>
