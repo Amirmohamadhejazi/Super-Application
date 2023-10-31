7a12ffc136563d2aa82469e7242483fa047eeece
Amirmohamad <Amirmohamadhejazi.9731@gmail.com>
2023-10-31 09:12:42 +0330

    fix: home and shop items page

--- src/app/data/data.tsx
+++ src/app/data/data.tsx
@@ -1,4 +1,25 @@
 import { IItemShop } from '../types/type';
-const dataItems: IItemShop[] = [];
+const dataItems: IItemShop[] = [
+    {
+        name: 'iphone 13',
+        brand: 'Apple',
+        numbers: 34
+    },
+    {
+        name: 's22',
+        brand: 'samsung',
+        numbers: 1
+    },
+    {
+        name: 'tg pro',
+        brand: 'lg',
+        numbers: 2
+    },
+    {
+        name: 'muiA',
+        brand: 'huawei',
+        numbers: 12
+    }
+];
 
 export { dataItems };
