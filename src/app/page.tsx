f2641b505321bbcd65203c544051998aebbc01ea
Amirmohamad <Amirmohamadhejazi.9731@gmail.com>
2023-12-20 09:25:59 +0330

    chore: fix link url github and fortnite to another project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,33 @@ import Link from 'next/link';
 export default function Home() {
     return (
         <div className="w-full min-h-screen font-bold flex justify-center gap-x-2 items-center">
-            {/* <Link href="/discord" className="hover:text-lg  transition-all duration-400">
-                Discord
-            </Link> */}
-            <Link href="/github" className="hover:text-lg  transition-all duration-400">
+            <Link
+                href="https://github-five-alpha.vercel.app"
+                target="_blank"
+                className="hover:text-lg  transition-all duration-400"
+            >
                 GithubStatus
             </Link>
-            <Link href="/fortnite" className="hover:text-lg  transition-all duration-400">
+            <Link
+                href="https://fortnite-rust.vercel.app"
+                target="_blank"
+                className="hover:text-lg  transition-all duration-400"
+            >
                 Fortnite
             </Link>
-            <Link href="/todoApp" className="hover:text-lg  transition-all duration-400">
+            <Link href="/todoApp" target="_blank" className="hover:text-lg  transition-all duration-400">
                 TodoApp
             </Link>
-            <Link href="/weather" className="hover:text-lg  transition-all duration-400">
+            <Link href="/weather" target="_blank" className="hover:text-lg  transition-all duration-400">
                 Weather
             </Link>
-            <Link href="/itemsShop?sortOrder=name" className="hover:text-lg   rounded-md transition-all duration-700">
+            {/* <Link
+                href="/itemsShop?sortOrder=name"
+                target="_blank"
+                className="hover:text-lg   rounded-md transition-all duration-700"
+            >
                 ShopItems
-            </Link>
+            </Link> */}
         </div>
     );
 }
